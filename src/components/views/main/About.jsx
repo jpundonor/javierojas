7d0003d3609959bfd826e8daf94eec572df87051
Javier <jardlml1209@gmail.com>
2022-05-16 18:28:54 -0400

    Added profile picture to the about

--- src/components/views/main/About.jsx
+++ src/components/views/main/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export const About = () => {
+  const profileImg = "/assets/img/javi2.jpg";
   return (
     <section className="">
       <div>
@@ -11,25 +12,25 @@ export const About = () => {
           <p>
             Hello! My name is Javier Rojas y me gusta crear experiecias
             digitales. Tengo como por objetivo trabajar idealmente de manera
-            autonoma, acercando la tecnologia a las pymes.
+            autonoma
           </p>
           <p>
             Aqui es donde tengo que rellenar con mas texto. Queda pendiente el
             about.
           </p>
-          <p>Aqui algunas de las tecnologías con las que trabajo:</p>
+          <p>Some of the technologies I work with:</p>
           <div className="about__skills-list">
             <div>Html</div>
             <div>Css</div>
             <div>Sass</div>
-            <div>JavaScrip</div>
+            <div>JavaScript</div>
             <div>React</div>
-            <div>Node.js</div>
+            <div>Next.js</div>
             <div>Tailwind</div>
           </div>
         </div>
-        <div className="">
-          <img src="" alt="" />
+        <div className="about__div-img">
+          <img className="about__img" src={profileImg} alt="Profile" />
         </div>
       </div>
     </section>
