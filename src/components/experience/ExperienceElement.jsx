a19b30b304d478e877f6d7fed2e03a2a65956419
Javier <jardlml1209@gmail.com>
2022-05-30 18:33:12 -0400

    A modal has been added for the experience with a carousel inside it

---
+++ src/components/experience/ExperienceElement.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { ElementPreview } from "./ElementPreview";
+
+export const ExperienceElement = ({
+  title,
+  link,
+  paragraph,
+  experiences = [],
+}) => {
+  const [elementModal, setElementModal] = useState(false);
+  const OpenInNew = "/assets/icons/open_in_new.svg";
+  const Folder = "/assets/icons/folder.svg";
+
+  function openModal() {
+    setElementModal(!elementModal);
+  }
+
+  return (
+    <>
+      <div className="experience__element">
+        <header className="experience__header-container">
+          <span>
+            <img
+              onClick={openModal}
+              className="experience__icon-folder"
+              src={Folder}
+              alt="Open Preview"
+            />
+          </span>
+          {link && (
+            <a
+              href={link}
+              aria-label="External Link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="experience__icon-external-link"
+                src={OpenInNew}
+                alt="External Link"
+              />
+            </a>
+          )}
+        </header>
+        <h3>{title}</h3>
+        <p>{paragraph}</p>
+        <div>
+          <footer className="experience__footer">
+            {experiences.map((exp, index) => (
+              <span key={index}>{exp}</span>
+            ))}
+          </footer>
+        </div>
+      </div>
+      {elementModal && <ElementPreview elementModal={elementModal} setElementModal={setElementModal} />}
+    </>
+  );
+};
