25b838bb82a4f4919ac10e9b79c745f68c6bdad7
Javier <jardlml1209@gmail.com>
2022-07-28 14:59:34 -0400

    Added the social list bar

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,6 +6,9 @@ import { Home } from "../home/Home";
 
 export const Main = () => {
   const arrowUp = "/assets/icons/arrow_circle_up.svg";
+  const github = "/assets/icons/icon-github.svg";
+  const insta = "/assets/icons/icon-instagram.svg";
+  const linked = "/assets/icons/icon-linkedin.svg";
   return (
     <div className="main__container">
       <Home />
@@ -15,6 +18,17 @@ export const Main = () => {
       <div className="main__arrow-up">
         <a href="/#home">
           <img src={arrowUp} alt="Up" />
+        </a>
+      </div>
+      <div className="main__social-list">
+        <a href="https://github.com/jardlml" target="_blank" rel="noopener noreferrer">
+          <img src={github} alt="Up" />
+        </a>
+        <a href="https://www.instagram.com/jardlml/" target="_blank" rel="noopener noreferrer">
+          <img src={insta} alt="Up" />
+        </a>
+        <a href="https://www.linkedin.com/in/javier-rojas-43324b143/" target="_blank" rel="noopener noreferrer">
+          <img src={linked} alt="Up" />
         </a>
       </div>
       <footer className="main__footer">{`Designed & Built by Javier Rojas`}</footer>
